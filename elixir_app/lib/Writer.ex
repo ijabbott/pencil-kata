5723d6ff6ed8a4d209293af48f50661b7327d34e
ijabbott <ij.abbott2306@gmail.com>
2023-02-08 22:22:15 -0600

    beginning of writer module

---
+++ elixir_app/lib/Writer.ex
@@ -0,0 +1,18 @@
+defmodule Writer do
+
+  @write_error_parameters {:error, "Writer.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
+
+  def write(_, text, _) when not is_binary(text) do
+    @write_error_parameters
+  end
+
+  def write(%Pencil{} = pencil, text, %Paper{} = paper) do
+    updatedText = paper.text <> text
+    paper = %{paper | text: updatedText}
+    {:ok, pencil, paper}
+  end
+
+  def write(_, _, _) do
+    @write_error_parameters
+  end
+end
