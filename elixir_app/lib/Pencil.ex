853f72d674c638f005b83660f333cda92c152609
ijabbott <ij.abbott2306@gmail.com>
2023-02-08 22:35:41 -0600

    Revert "refactor pencil to writer"
    This reverts commit aae21a18e510e8f03e6762ebddacd578d4101475.

--- elixir_app/lib/Pencil.ex
+++ elixir_app/lib/Pencil.ex
@@ -1,3 +1,33 @@
 defmodule Pencil do
-  defstruct durability: 0
+  @moduledoc """
+  Documentation for `Pencil`.
+  """
+
+  @doc """
+  Write
+
+  ## Examples
+
+      iex> Pencil.write(%Pencil{}, "Hello World!", %Paper{})
+      {:ok, %Pencil{}, %Paper{text: "Hello World!"}}
+
+  """
+
+  defstruct []
+
+  @write_error_parameters {:error, "Pencil.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
+
+  def write(_, text, _) when not is_binary(text) do
+    @write_error_parameters
+  end
+
+  def write(%Pencil{} = pencil, text, %Paper{} = paper) do
+    updatedText = paper.text <> text
+    paper = %{paper | text: updatedText}
+    {:ok, pencil, paper}
+  end
+
+  def write(_, _, _) do
+    @write_error_parameters
+  end
 end
--- elixir_app/test/pencil_test.exs
+++ elixir_app/test/pencil_test.exs
@@ -1,48 +1,41 @@
-defmodule WriterTest do
+defmodule PencilTest do
   use ExUnit.Case
-  doctest Writer
+  doctest Pencil
 
   test "when given a non-pencil as the first argument, write returns error describing proper input" do
     paper = %Paper{}
-    assert Writer.write("non-pencil", "Hello World!", paper) == {:error, "Writer.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
+    assert Pencil.write("non-pencil", "Hello World!", paper) == {:error, "Pencil.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
   end
 
   test "when given a non-string as the second argument, write returns error describing proper input" do
     paper = %Paper{}
     pencil = %Pencil{}
-    assert Writer.write(pencil, {:notString}, paper) == {:error, "Writer.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
+    assert Pencil.write(pencil, {:notString}, paper) == {:error, "Pencil.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
   end
 
   test "when given a non-paper as the third argument, write returns error describing proper input" do
     pencil = %Pencil{}
-    assert Writer.write(pencil, "Hello World!", "not paper") == {:error, "Writer.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
+    assert Pencil.write(pencil, "Hello World!", "not paper") == {:error, "Pencil.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
   end
 
   test "when given an empty string, write does not write on the paper" do
     paper = %Paper{}
     pencil = %Pencil{}
-    {:ok, _, updatedPaper} = Writer.write(pencil, "", paper)
+    {:ok, _, updatedPaper} = Pencil.write(pencil, "", paper)
     assert updatedPaper.text == ""
   end
 
   test "when given text, write returns the updated paper" do
     paper = %Paper{}
     pencil = %Pencil{}
-    {:ok, _, updatedPaper} = Writer.write(pencil, "Hello World!", paper)
+    {:ok, _, updatedPaper} = Pencil.write(pencil, "Hello World!", paper)
     assert updatedPaper.text == "Hello World!"
   end
 
   test "when given a pre-populated paper and text, write appends the given text to the paper" do
     paper = %Paper{text: "Hello "}
     pencil = %Pencil{}
-    {:ok, _, updatedPaper} = Writer.write(pencil, "World!", paper)
-    assert updatedPaper.text == "Hello World!"
-  end
-
-  test "when given text, write returns the updated paper" do
-    paper = %Paper{}
-    pencil = %Pencil{}
-    {:ok, _, updatedPaper} = Writer.write(pencil, "Hello World!", paper)
+    {:ok, _, updatedPaper} = Pencil.write(pencil, "World!", paper)
     assert updatedPaper.text == "Hello World!"
   end
 end
