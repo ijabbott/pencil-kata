aae21a18e510e8f03e6762ebddacd578d4101475
ijabbott <ij.abbott2306@gmail.com>
2023-02-08 22:30:02 -0600

    refactor pencil to writer

--- elixir_app/lib/Pencil.ex
+++ elixir_app/lib/Pencil.ex
@@ -1,33 +1,3 @@
 defmodule Pencil do
-  @moduledoc """
-  Documentation for `Pencil`.
-  """
-
-  @doc """
-  Write
-
-  ## Examples
-
-      iex> Pencil.write(%Pencil{}, "Hello World!", %Paper{})
-      {:ok, %Pencil{}, %Paper{text: "Hello World!"}}
-
-  """
-
-  defstruct []
-
-  @write_error_parameters {:error, "Pencil.write accepts the following parameters: write(%Pencil{}, binaryInput, %Paper{})"}
-
-  def write(_, text, _) when not is_binary(text) do
-    @write_error_parameters
-  end
-
-  def write(%Pencil{} = pencil, text, %Paper{} = paper) do
-    updatedText = paper.text <> text
-    paper = %{paper | text: updatedText}
-    {:ok, pencil, paper}
-  end
-
-  def write(_, _, _) do
-    @write_error_parameters
-  end
+  defstruct durability: 0
 end
